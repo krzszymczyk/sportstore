52c2e0f7cd627fbaf8a3403cf78b614e74a7d0ec
Krzysztof Szymczyk <krzysztof.szymczyk@yahoo.com>
2018-05-18 14:56:20 +0200

    Introduced EFCore repository

---
+++ SportStore/SportStore/Models/SeedData.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SportStore.Models
+{
+    public static class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder app)
+        {
+            ApplicationDbContext context = app.ApplicationServices.GetRequiredService<ApplicationDbContext>();
+            context.Database.Migrate();
+            if (!context.Products.Any())
+            {
+                context.Products.AddRange(
+                    new Product
+                    {
+                        Name = "Kajak",
+                        Description = "Łódka przeznaczona dla jednej osoby",
+                        Category = "Sporty wodne",
+                        Price = 275
+                    },
+                    new Product
+                    {
+                        Name = "Kamizelka ratunkowa",
+                        Description = "Łódka przeznaczona dla jednej osoby",
+                        Category = "Sporty wodne",
+                        Price = 48.95m
+                    },
+                    new Product
+                    {
+                        Name = "Piłka",
+                        Description = "Zatwierdzona przez FIFA rozmiar i waga",
+                        Category = "Piłka nożna",
+                        Price = 19.50m
+                    },
+                    new Product
+                    {
+                        Name = "Flagi narożne",
+                        Description = "Nadadzą twojemu boisku profesjonalny wygląd",
+                        Category = "Piłka nożna",
+                        Price = 34.95m
+                    },
+                    new Product
+                    {
+                        Name = "Stadion",
+                        Description = "Składany stadion na 35 000 osób",
+                        Category = "Piłka nożna",
+                        Price = 34.95m
+                    },
+                    new Product
+                    {
+                        Name = "Flagi narożne",
+                        Description = "Nadadzą twojemu boisku profesjonalny wygląd",
+                        Category = "Piłka nożna",
+                        Price = 75000
+                    },
+                    new Product
+                    {
+                        Name = "Czapka",
+                        Description = "Zwiększa efektywność mózgu o 75%",
+                        Category = "Szachy",
+                        Price = 16
+                    },
+                    new Product
+                    {
+                        Name = "Niestabilne krzesło",
+                        Description = "Zmniejsza szanse przeciwnika",
+                        Category = "Szachy",
+                        Price = 29.95m
+                    },
+                    new Product
+                    {
+                        Name = "Ludzka szachownica",
+                        Description = "Przyjemna gra dla całej rodziny",
+                        Category = "Szachy",
+                        Price = 75
+                    },
+                    new Product
+                    {
+                        Name = "Błyszczący król",
+                        Description = "Figura pokryta złotem i wysadzana diamientami",
+                        Category = "Szachy",
+                        Price = 1200
+                    }
+                );
+                context.SaveChanges();
+
+            }
+
+        }
+    }
+}
